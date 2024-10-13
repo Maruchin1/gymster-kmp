b30ec45c02c76bd37e4254f3648736f6e21cdff4
Marcin Piekielny <marcinpk.mp@gmail.com>
2024-10-13 15:41:45 +0200

    Add EmptyContent and LoadingContent

--- core/ui/src/commonMain/kotlin/com/maruchin/gymster/core/ui/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.maruchin.gymster.core.ui
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String = "Hello, ${platform.name}!"
-}
--- core/ui/src/commonMain/kotlin/com/maruchin/gymster/core/ui/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.maruchin.gymster.core.ui
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
