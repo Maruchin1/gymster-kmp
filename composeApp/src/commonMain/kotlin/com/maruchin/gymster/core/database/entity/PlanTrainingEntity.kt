1c43cfb9c3419078fae61a834fd3de5b3c251e9b
Marcin Piekielny <marcinpk.mp@gmail.com>
2024-10-26 15:06:26 +0200

    Update formatting

--- composeApp/src/commonMain/kotlin/com/maruchin/gymster/core/database/entity/PlanTrainingEntity.kt
+++ composeApp/src/commonMain/kotlin/com/maruchin/gymster/core/database/entity/PlanTrainingEntity.kt
@@ -4,4 +4,8 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-internal data class PlanTrainingEntity(@PrimaryKey val id: String, val planId: String, val name: String)
+internal data class PlanTrainingEntity(
+    @PrimaryKey val id: String,
+    val planId: String,
+    val name: String
+)
