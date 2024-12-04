5f9569ceea716fca9d5cdd6eae042710b5b23f49
Marcin Piekielny <marcinpk.mp@gmail.com>
2024-12-04 22:17:43 +0100

    Configure Detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     // this is necessary to avoid the plugins to be loaded multiple times
     // in each subproject's classloader
@@ -7,4 +9,19 @@ plugins {
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.detekt)
+}
+
+dependencies {
+    detektPlugins(libs.detekt.formatting)
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    autoCorrect = true
+    config.setFrom("$projectDir/detekt.yml")
+}
+
+tasks.withType<Detekt>().configureEach {
+    setSource(files(projectDir))
 }
